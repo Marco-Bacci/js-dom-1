45b0651a7fb7a6eb8ee145a74e5c237552656ac4
Marco Bacci <marcobacci94@gmail.com>
2025-07-09 19:41:00 +0200

    feat: (bonus)  completed

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const accendi = document.getElementById(`button`);
 const bulb = document.getElementById('lamp')
 // creo un event listener al click del bottone
 accendi.addEventListener('click', function(){
-  bulb.src= `img/yellow_lamp.png`
+  if (bulb.src.includes(`img/white_lamp.png`)){
+    bulb.src= `img/yellow_lamp.png`
+  }
+  else{
+    bulb.src= `img/white_lamp.png`
+  }
 });
-  

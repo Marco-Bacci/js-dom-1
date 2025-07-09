93a28bfe4f8277e86b436103e61f64b2cd4dbe94
Marco Bacci <marcobacci94@gmail.com>
2025-07-09 15:32:23 +0200

    build: event listener completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-const bulb = document.getElementById(`lamp`);
-bulb.src = `img/yellow_lamp.png`
+// dichiaro variabili del bottone e del img
+const accendi = document.getElementById(`button`);
+const bulb = document.getElementById('lamp')
+// creo un event listener al click del bottone
+accendi.addEventListener('click', function(){
+  bulb.src= `img/yellow_lamp.png`
+});
+  

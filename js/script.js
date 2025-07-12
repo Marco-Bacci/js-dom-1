da1b3d80fa9f24fee1a5cc1d0da0fbd1af89eba2
Marco Bacci <marcobacci94@gmail.com>
2025-07-12 17:51:27 +0200

    FEAT: bonus inner text to button

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const accendi = document.getElementById(`button`);
 const bulb = document.getElementById('lamp')
 // creo un event listener al click del bottone
 accendi.addEventListener('click', function(){
-  if (bulb.src.includes(`img/white_lamp.png`)){
-    bulb.src= `img/yellow_lamp.png`
+  if (bulb.src.includes(`img/yellow_lamp.png`)){
+    bulb.src= `img/white_lamp.png`
+    accendi.innerText = `ACCENDI`
   }
   else{
-    bulb.src= `img/white_lamp.png`
+    bulb.src= `img/yellow_lamp.png`
+    accendi.innerText = `SPEGNI`
+   
   }
 });
+
